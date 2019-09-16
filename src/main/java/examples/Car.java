58f2225a93331092edaece903973a4650baac75c
Bas Dijkstra <bas@ontestautomation.com>
2019-09-16 14:06:36 +0200

    Added examples, exercises and answers for REST Assured and (de-)serialization

--- src/main/java/examples/Car.java
+++ src/main/java/examples/Car.java
@@ -1,5 +1,7 @@
 package examples;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,9 +30,34 @@ public class Car {
         return String.format("I'm a %s %s %s", this.color, this.make, this.model);
     }
 
+    @JsonIgnore
     public boolean isItalian() {
 
         List<String> italianCarsList = Arrays.asList("Fiat", "Alfa Romeo", "Lancia", "Maserati", "Ferrari");
         return italianCarsList.contains(this.make);
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 }
---
+++ src/test/java/restassured/RestAssuredAnswers3Test.java
@@ -0,0 +1,71 @@
+package restassured;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import examples.Car;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.*;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static io.restassured.RestAssured.given;
+
+public class RestAssuredAnswers3Test {
+
+	private static RequestSpecification requestSpec;
+
+	@Rule
+	public WireMockRule wireMockRule = new WireMockRule(options().port(9876));
+
+	@BeforeClass
+	public static void createRequestSpecification() {
+
+		requestSpec = new RequestSpecBuilder().
+			setBaseUri("http://localhost").
+			setPort(9876).
+			setContentType(ContentType.JSON).
+			build();
+	}
+		
+	/*******************************************************
+	 * Create a new Car object that represents a blue Ford Focus
+	 * POST this object to /cars/postcar
+	 * Verify that the response HTTP status code is equal to 200
+	 ******************************************************/
+	
+	@Test
+	public void postCarObject_checkResponseHttpStatusCode_expect200() {
+
+		Car myCar = new Car("Ford", "Focus", "blue");
+
+		given().
+			spec(requestSpec).
+		and().
+			body(myCar).
+		when().
+			post("/car/postcar").
+		then().
+			assertThat().
+			statusCode(200);
+	}
+
+	/*******************************************************
+	 * Perform a GET to /cars/getcar/alfaromeogiulia
+	 * Store the response in a Car object
+	 * Verify, using that object, that the color is red
+	 * Use the standard Assert.assertEquals(expected,actual)
+	 * as provided by JUnit for the assertion
+	 ******************************************************/
+
+	@Test
+	public void getCarObject_checkColor_expectRed() {
+
+		Car myCar = given().
+			spec(requestSpec).
+			when().
+			get("/car/getcar/alfaromeogiulia").
+			as(Car.class);
+
+		Assert.assertEquals("red", myCar.getColor());
+	}
+}
