89ddea902ec3e643109a6112a9b95a271cec75be
Deepak Parari <dpak666@gmail.com>
2019-09-17 12:59:25 +0200

    Homework for day 2

--- src/test/java/homework/DataDrivenAlbumTest.java
+++ src/test/java/homework/DataDrivenAlbumTest.java
@@ -3,19 +3,33 @@ package homework;
 import com.tngtech.java.junit.dataprovider.DataProvider;
 import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 import com.tngtech.java.junit.dataprovider.UseDataProvider;
-import homeworkanswers.Album;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+@RunWith(DataProviderRunner.class)
 public class DataDrivenAlbumTest {
 
-    private Album album;
-
     // Refactor the test code from the previous exercise into a data driven test
-
     // Create a DataProvider method that returns a two-dimensional array containing the values required
     // for the tests in the previous exercise
+    private Album album;
+
+    @DataProvider
+    public static Object[][] albumDataProvider(){
+        return new Object[][]{
+                {"'Reverence'", "Faithless", true, "The Faithless album called 'Reverence' is their debut album"},
+                { "'Sunday 8PM'", "Faithless", false,"The Faithless album called 'Sunday 8PM' is not their debut album"}
+
+        };
+    }
+    @UseDataProvider("albumDataProvider")
+    @Test
+    public void dataDrivenAlbumStringReturnCheck(String title, String artist, boolean debutAlbum, String expectedResult){
+
+        album = new Album(title,artist,debutAlbum);
+        Assert.assertEquals(expectedResult,album.toString());
+    }
 
     // Create a data driven test that uses the values from the test data object returned by the data provider
     // to execute the test logic defined in the test method twice, once for each test data record
